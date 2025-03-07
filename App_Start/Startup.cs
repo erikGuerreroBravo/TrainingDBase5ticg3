e9268c6b18a83d1d7f77490362fcb0fe2679f5e3
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-07 09:57:37 -0600

    se agrega la clase startup para implementar la seguridad.

---
+++ App_Start/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(TrainingDBase5ticg3.App_Start.Startup))]
+
+namespace TrainingDBase5ticg3.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(
+                new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions() { 
+                
+                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                    LoginPath = new PathString("/Auth/Login")
+                });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            
+        }
+    }
+}
