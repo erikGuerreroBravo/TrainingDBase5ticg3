944d16ac1560c057dcc5b8d8cb3c7829251fff95
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-11 16:12:17 -0600

    se genera el panel del usuario con las caracteristicas de la sesion.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -56,6 +56,10 @@ namespace TrainingDBase5ticg3.Controllers
         }
 
 
-
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            return RedirectToAction("");
+        }
     }
 }
