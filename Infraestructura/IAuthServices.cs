a71af2e708cb222e40383ee5a9de1e21bff8b1f8
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-11 14:17:53 -0600

    se agrega el metodo de auteicacion de usuarios utilizando el metodo login

---
+++ Infraestructura/IAuthServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingDBase5ticg3.Models;
+
+namespace TrainingDBase5ticg3.Infraestructura
+{
+    public interface IAuthServices
+    {
+        Usuarios Login(string userName, string password);
+    }
+}
---
+++ Services/AuthServices.cs
@@ -0,0 +1,42 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using TrainingDBase5ticg3.Infraestructura;
+using TrainingDBase5ticg3.Models;
+
+namespace TrainingDBase5ticg3.Services
+{
+	public class AuthServices: IAuthServices
+    {
+        private readonly TestDbMensajeriaEntities db = null;
+        public AuthServices()
+        {
+            db = new TestDbMensajeriaEntities();
+        }
+
+        public Usuarios Login(string userName, string password) 
+        {
+            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password))
+            {
+                return null;
+            }
+
+            Usuarios usuario = db.Usuarios
+                .Include("UsuarioRol")
+                .FirstOrDefault(u => u.Email == userName);
+            if (usuario == null)
+            {
+                return null;
+            }
+            usuario.UsuarioRol = db.UsuarioRol.Where(u => u.IdUsuario == usuario.Id).ToList();
+            return usuario;
+
+        }
+
+       
+
+    }
+}
