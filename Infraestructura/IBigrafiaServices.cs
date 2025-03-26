7d45ff4b939e91be5f18a42e3c7ea55798c9b2ff
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-26 12:46:36 -0600

    se agrega  la biografia services para trabajar con la insercion de la biografia

---
+++ Infraestructura/IBigrafiaServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingDBase5ticg3.Infraestructura
+{
+    public interface IBigrafiaServices
+    {
+        bool InsertBiografia(string bio);
+    }
+}
--- Services/BigrafiaServices.cs
+++ Services/BigrafiaServices.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TrainingDBase5ticg3.Infraestructura;
+using TrainingDBase5ticg3.Models;
 
 namespace TrainingDBase5ticg3.Services
 {
-	public class BigrafiaServices
-	{
-	}
+	public class BigrafiaServices: IBigrafiaServices
+    {
+        private readonly TestDbMensajeriaEntities db = null;
+
+        public BigrafiaServices()
+        {
+            this.db = new TestDbMensajeriaEntities();
+        }
+
+        public bool InsertBiografia(string bio)
+        {
+            bool result = false;
+            var transaccion = this.db.Database.BeginTransaction();
+            try
+            {
+                Biografia biografia = new Biografia {FechaRegistro= DateTime.UtcNow, StrValor=bio };
+                this.db.Biografia.Add(biografia);
+                this.db.SaveChanges();
+                transaccion.Commit();
+                return result;
+            }
+            catch (Exception)
+            {
+                return result;              
+            }
+        }
+
+    }
 }
